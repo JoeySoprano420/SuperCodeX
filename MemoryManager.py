6183f642904271ff13c6520232ac6ef9007299fb
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-04-04 03:08:59 -0400

    Update MemoryManager.py

--- MemoryManager.py
+++ MemoryManager.py
@@ -8,3 +8,20 @@ class MemoryManager:
     def deallocate(self, var_name):
         if var_name in self.variables:
             del self.variables[var_name]
+
+class MemoryManager:
+    def __init__(self):
+        self.memory = {}
+
+    def allocate(self, var_name, size):
+        if var_name not in self.memory:
+            self.memory[var_name] = {'size': size, 'address': None}
+
+    def deallocate(self, var_name):
+        if var_name in self.memory:
+            del self.memory[var_name]
+
+    def cleanup(self):
+        for var_name in list(self.memory):
+            if self.memory[var_name]['address'] is None:
+                self.deallocate(var_name)
